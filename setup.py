fd4adf851a08d040250936bfce4e973b1b6796a6
youngsoo lee <prevdev@gmail.com>
2016-09-19 18:05:15 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name = 'jikji',
+	packages = ['jikji'],
+	version = '0.1.2',
+	description = 'Code-less static web generator based on RESTFul API Server',
+	license = 'MIT',
+
+	author = 'Youngsoo Lee',
+	author_email = 'prevdev@gmail.com',
+	
+	url = 'https://github.com/Prev/jikji',
+	keywords = ['static-web', 'generator'],
+
+	install_requires=[],
+)
